96ab062ba6e5e8de2bf22f8ac293faacf7773e0b
Riaan Marais <riaankrappie.marais5@gmail.com>
2024-08-03 17:47:13 +0200

    deleted duplicate files when we dragged and dropped

--- routes/checkoutRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { checkout } = require('../controllers/checkoutController');
-
-const router = express.Router();
-
-router.post('/', checkout);
-
-module.exports = router;
